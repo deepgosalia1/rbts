1646697594235f2f9716b0197b8a741add0b54a5
Deep Gosalia <32708932+deepgosalia1@users.noreply.github.com>
2021-11-21 03:58:23 -0600

    Navigation changes

--- src/components/ServerApi/index.js
+++ src/components/ServerApi/index.js
@@ -1,5 +1,9 @@
 import { callApi } from './callApi';
 
+export const loginUserAPI = async (uid,username, pass_hash) => {
+  return await callApi({endpoint:'login', method:'post', body:{uid,username,pass_hash}})
+}
+
 export const getBTCPrice = async (id, token) => {
   return await callApi({ endpoint: `https://api.coindesk.com/v1/bpi/currentprice.json`, fullUrl: true })
 };
