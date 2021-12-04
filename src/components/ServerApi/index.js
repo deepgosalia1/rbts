1adcf2dce3646b64d6b0b22e66402b92c550a732
Deep Gosalia <32708932+deepgosalia1@users.noreply.github.com>
2021-12-04 16:15:49 -0600

    changes

--- src/components/ServerApi/index.js
+++ src/components/ServerApi/index.js
@@ -30,12 +30,12 @@ export const placeTraderDependentTrade = async (cid, txamount, txtype, commtype,
   return await callApi({ endpoint: 'client/dependentTrade', method: 'post', body: { cid, txamount, txtype, commtype, txdate, currBTC: currBTCPriceinUSD } })
 };
 
-export const ApproveTrade = async (txid, currBTC, txtype, tid, commtype, cid) => {
+export const ApproveTrade = async (txid, currBTC, txtype, tid, commtype, cid, txdate, txamount) => {
   return await callApi({ endpoint: 'transactions/approveTrade', method: 'post', body: { txid, currBTC, txtype, tid, commtype, cid } })
 };
 
-export const RejectTrade = async (txid, txtype, tid, cid) => {
-  return await callApi({ endpoint: 'transactions/rejectTrade', method: 'post', body: { txid, txtype, tid, cid } })
+export const RejectTrade = async (txid, txtype, tid, cid, txdate, txamount) => {
+  return await callApi({ endpoint: 'transactions/rejectTrade', method: 'post', body: { txid, txtype, tid, cid, txdate, txamount } })
 };
 
 export const placeTopUpRequest = async (cid, fiatamount, txdate) => {
