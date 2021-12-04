556cffecc7a163f5566f21e2e0f4f589db174921
Deep Gosalia <32708932+deepgosalia1@users.noreply.github.com>
2021-12-04 16:17:30 -0600

    change

--- src/components/ServerApi/index.js
+++ src/components/ServerApi/index.js
@@ -30,8 +30,8 @@ export const placeTraderDependentTrade = async (cid, txamount, txtype, commtype,
   return await callApi({ endpoint: 'client/dependentTrade', method: 'post', body: { cid, txamount, txtype, commtype, txdate, currBTC: currBTCPriceinUSD } })
 };
 
-export const ApproveTrade = async (txid, currBTC, txtype, tid, commtype, cid, txdate, txamount) => {
-  return await callApi({ endpoint: 'transactions/approveTrade', method: 'post', body: { txid, currBTC, txtype, tid, commtype, cid } })
+export const ApproveTrade = async (txid, btc, txtype, tid, commtype, cid, txdate, txamount) => {
+  return await callApi({ endpoint: 'transactions/approveTrade', method: 'post', body: { txid, currBTC: btc, txtype, tid, commtype, cid } })
 };
 
 export const RejectTrade = async (txid, txtype, tid, cid, txdate, txamount) => {
